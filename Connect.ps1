b2939b6719305c898ae296a6af287e688dcb590b
Manny Bonorand <mbonorand@gmail.com>
2024-07-13 15:12:54 -0700

    updated Build, remove and config

--- Connect.ps1
+++ Connect.ps1
@@ -1,9 +1,9 @@
 param(
-    [ValidateSet("192.168.100.80","192.168.100.81","192.168.100.82")]
+    [ValidateSet("192.168.100.81","192.168.100.84","192.168.100.85")]
     [string]$node
 )
 
-$secrets = Get-Content -Path "$PSScriptRoot\secrets.json" | ConvertFrom-Json
+$secrets = Get-Content -Path "$PSScriptRoot\configs\secrets.json" | ConvertFrom-Json
 
 $login = $secrets.Username + "!" + $secrets.Token_Name + "=" + $secrets.API_Token
 
--- Setup.ps1
+++ Setup.ps1
@@ -2,8 +2,7 @@
 Write-Host "PS-Proxmox Setup"
 
 $moduleName = 'Corsinvest.ProxmoxVE.Api'
-$sourcePath = "$PSScriptRoot\env.template.json"
-$jsonPath = "$PSScriptRoot\env.json"
+$jsonPath = "$PSScriptRoot\configs\env.json"
 
 
 
@@ -16,38 +15,3 @@ if (-not (Get-Module -ListAvailable -Name $moduleName)) {
 } else {
     Write-Host "$moduleName is already installed"
 }
-
-# Update Connect.ps1 with relevant Node information.
-
-# Copy the env.template.json to env.json if it doesn't already exist
-if (-not (Test-Path -Path $jsonPath)) {
-    Copy-Item -Path $sourcePath -Destination $jsonPath -Verbose
-} else {
-    Write-Host "$jsonPath Already exists"
-}
-
-# Read and parse the JSON file
-$jsonContent = Get-Content -Path $jsonPath | ConvertFrom-Json
-$jsonValues = $jsonContent.Nodes.PSObject.Properties.Value.IP
-
-# Construct the new ValidateSet string
-$validateSetString = '[ValidateSet("' + ($jsonValues -join '","') + '")]'
-
-# Read the Connect.ps1 script content
-$scriptContent = Get-Content -Path $connectScriptPath -Raw
-
-# Regex to find the existing ValidateSet attribute
-$validateSetPattern = '\[ValidateSet\((.*)\)\]'
-$hasValidateSet = $scriptContent -match $validateSetPattern
-
-# Update the ValidateSet in the script
-if ($hasValidateSet) {
-    $updatedScriptContent = $scriptContent -replace $validateSetPattern, $validateSetString
-} else {
-    # If no ValidateSet exists, prepend the new ValidateSet to the script
-    $updatedScriptContent = $validateSetString + "`n" + $scriptContent
-}
-
-# Write the updated content back to Connect.ps1
-Set-Content -Path $connectScriptPath -Value $updatedScriptContent -Verbose
-
