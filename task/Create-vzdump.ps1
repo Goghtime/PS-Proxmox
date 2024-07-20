718bf7546028b8c63c50d0cfe7cc776673ff7b97
Manny Bonorand <mbonorand@gmail.com>
2024-07-20 10:52:22 -0700

    updated relative paths

--- task/Create-vzdump.ps1
+++ task/Create-vzdump.ps1
@@ -11,9 +11,9 @@ param(
 $rootPath = Split-Path -Path $PSScriptRoot -Parent
 
 # Import Function -force is required to run multiple times in a row.
-Import-Module "D:\Projects\PS-Proxmox\functions\Invoke-ProxmoxApiPOST.ps1" -Force -DisableNameChecking
-$secrets = Get-Content -Path "D:\Projects\PS-Proxmox\env\secrets.json" | ConvertFrom-Json
-$ConfigPath = Get-Content -Path "D:\Projects\PS-Proxmox\configs\$ConfigFile.json" | ConvertFrom-Json
+Import-Module "$rootPath\functions\Invoke-ProxmoxApiPOST.ps1" -Force -DisableNameChecking
+$secrets = Get-Content -Path "$rootPath\PS-Proxmox\env\secrets.json" | ConvertFrom-Json
+$ConfigPath = Get-Content -Path "$rootPath\PS-Proxmox\configs\$ConfigFile.json" | ConvertFrom-Json
 
 # Define the body for the vzdump command
 $body = @{
