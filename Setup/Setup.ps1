b6479157b6ec389791bf3144a85e3de6f767421f
Manny Bonorand <mbonorand@gmail.com>
2024-07-20 10:50:43 -0700

    Update .gitignore and Setup/Setup.ps1 with necessary changes

--- Setup/Setup.ps1
+++ Setup/Setup.ps1
@@ -1,14 +1,17 @@
 Write-Host "PS-Proxmox Setup"
 
 $rootPath = Split-Path -Path $PSScriptRoot -Parent
-$folderPath = "$rootPath/env"
+$folderPaths = @("$rootPath/env", "$rootPath/logs", "$rootPath/vars")
 
-if (-Not (Test-Path -Path $folderPath)) {
-    New-Item -ItemType Directory -Path $folderPath
-    Write-Host "Folder created: $folderPath"
-} else {
-    Write-Host "Folder already exists: $folderPath"
+foreach ($folderPath in $folderPaths) {
+    if (-Not (Test-Path -Path $folderPath)) {
+        New-Item -ItemType Directory -Path $folderPath
+        Write-Host "Folder created: $folderPath"
+    } else {
+        Write-Host "Folder already exists: $folderPath"
+    }
 }
+
 
 & "$rootPath\setup\managejson-secrets.ps1"
 
